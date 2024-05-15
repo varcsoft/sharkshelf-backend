02693e696c8f324ce47ec5895131e711d2a08c8b
Kokkula Shivakumar Mallesh <kokkulashivakumar1@gmail.com>
2024-05-16 00:50:35 +0530

    commit

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.options('*', cors());
+app.get('/', async (req, res) => res.send('Hello! Welcome To SHARKSHELF'));
 initprisma();
 initRoutes(app);
 initServer(app);
